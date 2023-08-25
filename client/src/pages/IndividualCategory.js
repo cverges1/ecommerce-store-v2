7ae2edefa37d7cebf9658a406a7fc1ae699b64a0
Trenton_Sallee <tretnonsmiles@gmail.com>
2023-08-24 21:27:51 -0700

    Singleproduct page edited, nav edited

---
+++ client/src/pages/IndividualCategory.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function ProductSection({ category }) {
+  return (
+    <section className="product-section">
+      <h2 className="text-align-center category-title">{category.name}</h2>
+      <div className="card-container">
+        {category.categoryProducts.map((Product) => (
+          <div key={Product.id} className="card">
+            <Link to={`/product/${Product.id}`} className="product-a-tags">
+              <img src={Product.image} alt="" className="product-img" />
+              <div className="product-text">
+                <p className="text-align-center product-name">{Product.name}</p>
+                <p className="text-align-center product-price">{Product.price}</p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default ProductSection;
