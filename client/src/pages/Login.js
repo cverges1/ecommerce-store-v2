eb152d4244fa76f96c2eaea564a421fdc25148b5
Connor <99grogan@gmail.com>
2023-08-24 20:35:59 -0700

    Login work

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,21 +8,44 @@ function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN);
 
-  const handleFormSubmit = async (event) => {};
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const { data } = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+  
+      if (data && data.login && data.login.token) {
+        const token = data.login.token;
+        Auth.login(token);
+      } else {
+        console.error("Token not received in the response.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error.message);
+    }
+  };
 
-  const handleChange = (event) => {};
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState(previousState => ({
+      ...previousState,
+      [name]: value,
+    }));
+  };
 
   return (
     <div class="wrapper-div flex-column w-50 align-items-center justify-content-center">
       <h2>Sign in to your account</h2>
-      <form id="sign-in-form">
+      <form id="sign-in-form" onSubmit={handleFormSubmit}>
         <div class="flex-column">
           <label for="sign-in-email">Email:</label>
-          <input id="sign-in-email" type="text" placeholder="" />
+          <input id="sign-in-email" type="text" placeholder="" onChange={handleChange} />
         </div>
         <div class="flex-column">
           <label for="sign-in-password">Password:</label>
-          <input id="sign-in-password" type="password" placeholder="" />
+          <input id="sign-in-password" type="password" placeholder="" onChange={handleChange} />
         </div>
         <button type="submit">SIGN IN</button>
       </form>
