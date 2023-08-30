21dad0de17d4d4b835e8826a56cf40e6def7ac72
lugo.kse23@gmail.com <lugo.kse23@gmail.com>
2023-08-29 21:41:37 -0700

    added more to reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -5,8 +5,9 @@ import {
     UPDATE_CATEGORIES,
     UPDATE_SINGLE_CATEGORY,
     ADD_TO_CART,
+    REMOVE_FROM_CART,
     UPDATE_CART_QUANTITY,
-    REMOVE_FROM_CART
+    CLEAR_CART
 
 } from "./actions";
 
@@ -31,7 +32,23 @@ export const reducer = (state, action) => {
             return {
                 ...state,
                 currentCategory: action.currentCategory
-            }
+            };
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter(product => {
+            return product._id !== action._id;
+            });
+            return {
+                ...state,
+            cartOpen: newState.length > 0,
+            cart: newState
+            };
+        case CLEAR_CART:
+            return {
+          ...state,
+          cartOpen: false,
+          cart: []
+            };
+  
         case UPDATE_CART_QUANTITY:
             return {
                 ...state,
