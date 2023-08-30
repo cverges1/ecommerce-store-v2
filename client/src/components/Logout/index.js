dff1084d5c0941e1d875c0783ce8cb0202a46681
cverges1 <122914738+cverges1@users.noreply.github.com>
2023-08-29 22:10:11 -0700

    logout button is working

---
+++ client/src/components/Logout/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Auth from "../../utils/auth";
+
+function Logout() {
+  return (
+    <a href="/" onClick={() => Auth.logout()}>
+      Logout
+    </a>
+  );
+}
+
+export default Logout;
