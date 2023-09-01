829833c9e1fae7df5530e549bd019c0636deb82a
cverges1 <122914738+cverges1@users.noreply.github.com>
2023-08-31 18:36:02 -0700

    updated Bottom Div key

--- client/src/components/BottomDivSection/index.js
+++ client/src/components/BottomDivSection/index.js
@@ -9,7 +9,6 @@ function BottomDivSection() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  const products = data.products;
   const newProducts = [data.products[0], data.products[1],data.products[2],data.products[3]]
 
   return (
@@ -17,7 +16,7 @@ function BottomDivSection() {
       <h3 className="text-align-center section-title">New Arrivals</h3>
       <div className="flex-row w-100">
         {newProducts.map((product) => (
-          <div className="flex-item col card" id="responsive-cardss" key={product.id}>
+          <div className="flex-item col card" id="responsive-cardss" key={product._id}>
             <Link to={`/product/${product._id}`} className="product-a-tags">
               <img src={product.image} alt="" className="product-img" />
               <div className="text-bottom">
